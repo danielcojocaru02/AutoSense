0657381a2a90063d02d071493225884f206b52b3
Daniel Cojocaru <109951979+danielcojocaru02@users.noreply.github.com>
2025-02-17 20:13:03 +0200

    Configured project for building apk for physical devices Android

---
+++ ios/build/ExplicitPrecompiledModules/flutter_blue_plus_darwin-47A59GVD7WADUEQ4PGTIJ69RS.d
@@ -0,0 +1,8 @@
+moduledependenciestarget: \
+  /Users/danielcojocaru/Documents/licenta/auto_app_problems/build/ios/Debug-iphonesimulator/flutter_blue_plus_darwin/flutter_blue_plus_darwin.framework/Modules/module.modulemap \
+  /Users/danielcojocaru/Documents/licenta/auto_app_problems/build/ios/Debug-iphonesimulator/flutter_blue_plus_darwin/flutter_blue_plus_darwin.framework/Headers/flutter_blue_plus_darwin-umbrella.h \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator18.1.sdk/System/Library/Frameworks/UIKit.framework/Modules/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator18.1.sdk/usr/include/TargetConditionals.modulemap \
+  /Users/danielcojocaru/Documents/licenta/auto_app_problems/build/ios/Debug-iphonesimulator/flutter_blue_plus_darwin/flutter_blue_plus_darwin.framework/Headers/FlutterBluePlusPlugin.h \
+  /Users/danielcojocaru/Documents/licenta/auto_app_problems/build/ios/Debug-iphonesimulator/Flutter.framework/Modules/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator18.1.sdk/System/Library/Frameworks/CoreBluetooth.framework/Modules/module.modulemap
---
+++ ios/build/Pods.build/Debug-iphonesimulator/flutter_blue_plus_darwin.build/Objects-normal/x86_64/FlutterBluePlusPlugin.d
@@ -0,0 +1,9 @@
+dependencies: \
+  /Users/danielcojocaru/Documents/licenta/auto_app_problems/ios/build/Pods.build/Debug-iphonesimulator/flutter_blue_plus_darwin.build/module.modulemap \
+  /Users/danielcojocaru/.pub-cache/hosted/pub.dev/flutter_blue_plus_darwin-3.0.0/darwin/flutter_blue_plus_darwin/Sources/flutter_blue_plus_darwin/FlutterBluePlusPlugin.m \
+  /Users/danielcojocaru/Documents/licenta/auto_app_problems/ios/Pods/Target\ Support\ Files/flutter_blue_plus_darwin/flutter_blue_plus_darwin-prefix.pch \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library/Frameworks/UIKit.framework/Modules/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/include/TargetConditionals.modulemap \
+  /Users/danielcojocaru/.pub-cache/hosted/pub.dev/flutter_blue_plus_darwin-3.0.0/darwin/flutter_blue_plus_darwin/Sources/flutter_blue_plus_darwin/./include/flutter_blue_plus_darwin/FlutterBluePlusPlugin.h \
+  /Users/danielcojocaru/Documents/flutter/bin/cache/artifacts/engine/ios/Flutter.xcframework/ios-arm64_x86_64-simulator/Flutter.framework/Modules/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library/Frameworks/CoreBluetooth.framework/Modules/module.modulemap
